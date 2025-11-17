abb8f2ea789778328a43c60975801c5a68b78db4
jaydeale <jaydeale03@gmail.com>
2025-11-18 03:10:15 +1100

    Added retieve func

--- club_results/main.py
+++ club_results/main.py
@@ -1,38 +1,32 @@
 score_dict = {}
 
 def retrieve_scores():
-    print("Working on this")
+    try:
+        for player, score in score_dict.items(): # itterate over items in score dict
+            yield player, score # return them one by one
+    except Exception as e:
+        print(f"An Error Occoured: {e}")
 
 def add_player():
     try:
-        #Player's name loop & validility
-        while True:
-            player_name = input("Enter The Players Full Name: ")
-            if all(char.isalpha() or char.isspace() for char in player_name) and player_name.strip():
+        while True: #Player name loop & validility
+            player_name = input("Enter The Players Full Name: ") #Retrieve Input
+            if all(char.isalpha() or char.isspace() for char in player_name) and player_name.strip(): #Validate Input
                 break
             else:
                  print("Please enter a valid player's name.")
-
-        #Player's score loop & validility
-        while True:
-            player_score = input("Enter a players score: ")
-            if player_score and player_score.isdigit():
-                player_score = int(player_score)
-                if player_score <= 100 and player_score >= 1:
+        while True: #Player score loop & validility
+            player_score = input("Enter a players score: ") #Retrieve Input
+            if player_score and player_score.isdigit(): #Validate Input
+                player_score = int(player_score) #Convert Input
+                if player_score <= 100 and player_score >= 1:#Validate again
                     break
                 else:
                     print("Please enter a valid score between 1-100")
             else:
                 print("Please enter a valid score")
-
-        score_dict[player_name] = player_score
-
-        new_player = input("Do you want to add another player? (Y/N)")
-        if new_player.lower() == "y" or new_player.lower() == "yes":
-            return add_player()
-        elif new_player.lower() == "n" or new_player.lower() == "no":
-            return
-        return print(f"Added {player_name}")
+        score_dict[player_name] = player_score #Add to dict "PlayerName:PlayerScore"
+        return "Completed"
     except Exception as e:
         print(f"An Error Occoured: {e}")
 
@@ -40,14 +34,18 @@ def main():
     while True:
         print("=======")
         print("(1) Add Player")
-        print("(2)")
+        print("(2) Display Player")
         print("(q) Quit")
         print("=======")
         choice = input("Enter Choice: ")
         if choice == "1":
             add_player()
+            # need to add the y/n i can revert it back the old way but it should be in here u can set
+            # add_player to a variable and if it equals "Completed" (currently can change that upove) and check that way idk
         elif choice == "2":
-            retrieve_scores()
+            print("Player's Name | Player's Score")
+            for player,score in retrieve_scores(): # display the values returned
+                print(f"{player} | {score}") # you will need to fix the display just here for testing
         elif choice.lower() == "q":
             print("Goodbye")
             break
