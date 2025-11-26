3972d00588e20040cc9662ee40b287b58914a628
jaydeale <jaydeale03@gmail.com>
2025-11-26 23:41:53 +1100

    Final fixes, Updated Code

--- distance_calc/main.py
+++ distance_calc/main.py
@@ -7,27 +7,26 @@ def show_results(speed, time):#Func to display results to user
     for minute in range(1, time + 1):#For every minute calculate the distance and display it
         distance = speed * minute#Calculate the result
         print(f"{str(minute):^15} | {str(distance):^15}")#Print to user
+
 def main():#Main function loop
     while True:
         try:
             speed=int(input("Enter the speed of the ball (in meters per minute): "))#Get user input for distance
-            if not speed:#Check if there is input
-                print("Please enter a value.")
-                main()#Restart the loop
-                break#Break current loop
             time=int(input("How many minutes has the ball travelled: "))#Get user input for time
-            if not time:#Check if there is input
-                print("Please enter a value.")
-                main()#Restart the loop
-                break#Break current loop
+
             assert speed >= 0#Check if input is a negative number
             assert time >= 0#Check if input is a negative number
+
             show_results(speed, time)#Send result
         except TypeError:#Check if wrong type
             print("Invalid Input: Please input a number.")
-        except AssertionError:#Check for negative number
-            print("Invalid Input: Please only enter positive numbers.")
+        except ValueError:
+            print("Invalid Input: Please input the correct value.")
+        except KeyboardInterrupt:
+            print("Goodbye")
+            break
         except ZeroDivisionError:#Check for Zero Division
             print("Error: Division by Zero is not allowed.")
+
 if __name__ == "__main__":
     main()
